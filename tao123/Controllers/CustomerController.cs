6d55c1b81d55995483923bd56757d2c8e8726946
Ali Raza Akhtar <alix0731@stud.kea.dk>
2021-05-16 16:21:26 +0200

    Add project files.

---
+++ tao123/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tao123.Models;
+
+namespace tao123.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+            Customer customer = new Customer { Name = "Ali" };
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return View(customer);
+        }
+
+       
+    }
+}
---
+++ tao123/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tao123.Startup))]
+namespace tao123
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
